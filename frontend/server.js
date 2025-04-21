f2192792ba636b01d91d351b06099c35fdc61397
RavinderDev_09 <its.ravinder000@gmail.com>
2025-04-21 13:39:09 +0530

    first commit

---
+++ frontend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3002;
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default route to serve upload.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+console.log('add');
+
+
+app.listen(PORT, () => {
+  console.log(`Frontend form running at http://localhost:${PORT}`);
+});
+console.log("7778");
