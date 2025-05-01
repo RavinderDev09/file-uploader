cd1921be7ecb24c40a62c08c87eae99040fc92b0
RavinderDev_09 <its.ravinder000@gmail.com>
2025-05-01 16:16:24 +0530

    last cm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,34 @@
-import { Module } from '@nestjs/common';
-import { FileSharingModule } from './file-sharing/file-sharing.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+  import { Module } from '@nestjs/common';
+  import { FileSharingModule } from './file-sharing/file-sharing.module';
+  import { MongooseModule } from '@nestjs/mongoose';
+  import { ConfigModule, ConfigService } from '@nestjs/config';
+  import { UsersModule } from './users/users.module';
+  import { AuthModule } from './auth/auth.module';
 
 
-@Module({
-  imports: [FileSharingModule,AuthModule,
-    ConfigModule.forRoot({ isGlobal: true }),  // Ensure env variables are loaded
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI'),
+
+  @Module({
+    imports: [FileSharingModule,AuthModule,
+      ConfigModule.forRoot({ isGlobal: true,   envFilePath: '.env' }),  // Ensure env variables are loaded
+      // MongooseModule.forRootAsync({
+      //   imports: [ConfigModule],
+      //   useFactory: async (configService: ConfigService) => ({
+      //     uri: configService.get<string>('MONGO_URI'),
+      //   }),
+      //   inject: [ConfigService],
+      // }), 
+      MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => {
+          console.log('Mongo URI:', configService.get<string>('MONGO_URI'));
+          return {
+            uri: configService.get<string>('MONGO_URI'),
+          };
+        },
+        inject: [ConfigService],
       }),
-      inject: [ConfigService],
-    }),  ],
-  providers: [],
-})
-export class AppModule {}
-    
+     ],
+    providers: [],
+  })
+  export class AppModule {}
+      
